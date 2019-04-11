70d76af75706b908efa2dfc6771fe59f15ec791b
Lasha Krikheli <lasha84@gmail.com>
2019-04-11 04:18:47 -0400

    all the changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+import './Default.css';
+
+import Home from './components/Home'
+import Error from './components/Error'
+import Nav from './starWars/Nav'
+import Vehicle from './starWars/Vehicle'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Awesome Star Wars Vehicles</h1>
+        <Router>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+          </ul>
+          <hr />
+          <Nav />
+          <hr />
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+            <Route path="/vehicle/:vehicleId" component={Vehicle} />
+
+            <Route component={Error} />
+          </Switch>
+        </Router>
       </div>
     );
   }
